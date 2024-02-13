637361944dc1bf6d9acdfd8c3c65c7fa8a6e232f
ACGaming <4818419+ACGaming@users.noreply.github.com>
2024-02-13 20:10:07 +0100

    Update project, fix OptiFine's custom sky

--- src/main/java/chylex/respack/ResourcePackOrganizer.java
+++ src/main/java/chylex/respack/ResourcePackOrganizer.java
@@ -1,5 +1,8 @@
 package chylex.respack;
-import java.util.Map;
+
+import chylex.respack.ConfigHandler.DisplayPosition;
+import chylex.respack.gui.GuiCustomResourcePacks;
+import chylex.respack.render.RenderPackListOverlay;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiScreenResourcePacks;
 import net.minecraftforge.client.event.GuiOpenEvent;
@@ -15,66 +18,62 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.network.NetworkCheckHandler;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
-import chylex.respack.ConfigHandler.DisplayPosition;
-import chylex.respack.gui.GuiCustomResourcePacks;
-import chylex.respack.render.RenderPackListOverlay;
-import chylex.respack.repository.ResourcePackRepositoryCustom;
+
+import java.util.Map;
 
 @Mod(modid = ResourcePackOrganizer.MODID, name = ResourcePackOrganizer.MODNAME, useMetadata = true, dependencies = "required-after:forge@[14.21.1.2387,)", guiFactory = "chylex.respack.gui.GuiModConfig")
-public final class ResourcePackOrganizer{
-	public static final String MODID = "resourcepackorganizer";
-	public static final String MODNAME = "Resource Pack Organizer";
-	
-	private static ConfigHandler config;
-	
-	public static ConfigHandler getConfig(){
-		return config;
-	}
-	
-	@EventHandler
-	public void onPreInit(FMLPreInitializationEvent e){
-		if (FMLCommonHandler.instance().getSide() == Side.SERVER){
-			FMLLog.bigWarning(MODNAME+" cannot be installed on a server!");
-			FMLCommonHandler.instance().exitJava(1, false);
-		}
-		
-		config = new ConfigHandler(e.getSuggestedConfigurationFile());
-		onConfigLoaded();
-		
-		MinecraftForge.EVENT_BUS.register(this);
-		
-		ResourcePackRepositoryCustom.overrideRepository(config.options.getEnabledPacks());
-		RenderPackListOverlay.refreshPackNames();
-	}
-	
-	@NetworkCheckHandler
-	public boolean onNetworkCheck(Map<String, String> versions, Side side){
-		return true;
-	}
-	
-	@SubscribeEvent(priority = EventPriority.LOWEST, receiveCanceled = true)
-	@SideOnly(Side.CLIENT)
-	public void onGuiOpen(GuiOpenEvent e){
-		if (e.getGui() != null && e.getGui().getClass() == GuiScreenResourcePacks.class){
-			e.setGui(new GuiCustomResourcePacks(Minecraft.getMinecraft().currentScreen));
-		}
-	}
-	
-	@SubscribeEvent
-	@SideOnly(Side.CLIENT)
-	public void onConfigChanged(OnConfigChangedEvent e){
-		if (e.getModID().equals(MODID)){
-			config.reload();
-			onConfigLoaded();
-		}
-	}
-	
-	private void onConfigLoaded(){
-		if (config.options.getDisplayPosition() == DisplayPosition.DISABLED){
-			RenderPackListOverlay.unregister();
-		}
-		else{
-			RenderPackListOverlay.register();
-		}
-	}
+public final class ResourcePackOrganizer {
+    public static final String MODID = "resourcepackorganizer";
+    public static final String MODNAME = "Resource Pack Organizer";
+
+    private static ConfigHandler config;
+
+    public static ConfigHandler getConfig() {
+        return config;
+    }
+
+    @EventHandler
+    public void onPreInit(FMLPreInitializationEvent e) {
+        if (FMLCommonHandler.instance().getSide() == Side.SERVER) {
+            FMLLog.bigWarning(MODNAME + " cannot be installed on a server!");
+            FMLCommonHandler.instance().exitJava(1, false);
+        }
+
+        config = new ConfigHandler(e.getSuggestedConfigurationFile());
+        onConfigLoaded();
+
+        MinecraftForge.EVENT_BUS.register(this);
+
+        RenderPackListOverlay.refreshPackNames();
+    }
+
+    @NetworkCheckHandler
+    public boolean onNetworkCheck(Map<String, String> versions, Side side) {
+        return true;
+    }
+
+    @SubscribeEvent(priority = EventPriority.LOWEST, receiveCanceled = true)
+    @SideOnly(Side.CLIENT)
+    public void onGuiOpen(GuiOpenEvent e) {
+        if (e.getGui() != null && e.getGui().getClass() == GuiScreenResourcePacks.class) {
+            e.setGui(new GuiCustomResourcePacks(Minecraft.getMinecraft().currentScreen));
+        }
+    }
+
+    @SubscribeEvent
+    @SideOnly(Side.CLIENT)
+    public void onConfigChanged(OnConfigChangedEvent e) {
+        if (e.getModID().equals(MODID)) {
+            config.reload();
+            onConfigLoaded();
+        }
+    }
+
+    private void onConfigLoaded() {
+        if (config.options.getDisplayPosition() == DisplayPosition.DISABLED) {
+            RenderPackListOverlay.unregister();
+        } else {
+            RenderPackListOverlay.register();
+        }
+    }
 }
--- src/main/java/chylex/respack/packs/ResourcePackListEntryCustom.java
+++ src/main/java/chylex/respack/packs/ResourcePackListEntryCustom.java
@@ -1,26 +1,27 @@
 package chylex.respack.packs;
+
+import chylex.respack.gui.GuiCustomResourcePacks;
 import net.minecraft.client.resources.ResourcePackListEntryFound;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
-import chylex.respack.gui.GuiCustomResourcePacks;
 
 @SideOnly(Side.CLIENT)
-public abstract class ResourcePackListEntryCustom extends ResourcePackListEntryFound{
-	public ResourcePackListEntryCustom(GuiCustomResourcePacks ownerScreen){
-		super(ownerScreen, null);
-	}
-	
-	@Override
-	public abstract void bindResourcePackIcon();
-	
-	@Override
-	public abstract String getResourcePackName();
-	
-	@Override
-	public abstract String getResourcePackDescription();
-	
-	@Override
-	public boolean showHoverOverlay(){
-		return super.showHoverOverlay();
-	}
+public abstract class ResourcePackListEntryCustom extends ResourcePackListEntryFound {
+    public ResourcePackListEntryCustom(GuiCustomResourcePacks ownerScreen) {
+        super(ownerScreen, null);
+    }
+
+    @Override
+    public abstract void bindResourcePackIcon();
+
+    @Override
+    public abstract String getResourcePackName();
+
+    @Override
+    public abstract String getResourcePackDescription();
+
+    @Override
+    public boolean showHoverOverlay() {
+        return super.showHoverOverlay();
+    }
 }
